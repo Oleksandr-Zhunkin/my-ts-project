4c5d5222849118539834378c57d6de75b3296f2a
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-07-09 14:29:25 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -295,11 +295,3 @@ import "./style.css";
 //   return loggedFunction;
 // }
 // export {};
-
-const age: number = 50;
-const username: string = "Max";
-const toggle: boolean = true;
-const empty: null = null;
-const callback = (a: number) => {
-  return 100 + a;
-};
